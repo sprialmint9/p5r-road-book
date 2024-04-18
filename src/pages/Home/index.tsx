ad2dd9d0d0cd5105e3feb2c40245542f4907c2d5
zhiwei.guo <867321200@qq.com>
2024-04-18 09:07:22 +0800

    fix: 日期展示添加sticky

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ function Home() {
     <>
       <Navbar />
       <div className="pl-4 pr-4 pt-18 sm:grid sm:grid-cols-2 sm:gap-4 lg:w-1000px ml-a mr-a">
-        <div>
+        <div className="sticky">
           <DateSwipe />
         </div>
         <div>
